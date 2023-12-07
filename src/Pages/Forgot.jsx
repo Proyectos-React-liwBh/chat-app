00b473d6acbe56ac51a3fe63152e0809ac6abbde
liwBh <liwbarqueroh@gmail.com>
2023-12-07 17:32:14 -0600

    animaciones forgot

--- src/Pages/Forgot.jsx
+++ src/Pages/Forgot.jsx
@@ -1,7 +1,19 @@
+import asteroide from "../assets/Image/asteroide.png";
+import asteroide2 from "../assets/Image/asteroide2.png";
+import asteroide3 from "../assets/Image/asteroide3.png";
+
 const Forgot = () => {
   return (
-    <div className="bg-forgot min-vh-100">
+    <div className="bg-forgot min-vh-100 overflow-hidden">
       <h1>Forgot</h1>
+
+      
+      <div>
+        <img src={asteroide} alt="Asteroide"  className="asteroid first"/>
+        <img src={asteroide2} alt="Asteroide"  className="asteroid second"/>
+        <img src={asteroide3} alt="Asteroide"  className="asteroid third "/>
+      </div>
+     
     </div>
   );
 };
